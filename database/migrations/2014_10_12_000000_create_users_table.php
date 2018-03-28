472db709e134b0608c27e387c6357bd7946ca0cd
zsleader <522376982@qq.com>
2018-03-28 08:42:49 +0000

    userInfo done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,17 +11,17 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+     public function up()
+     {
+         Schema::create('users', function (Blueprint $table) {
+             $table->increments('id');
+             $table->string('name');
+             $table->string('email')->unique();
+             $table->string('password');
+             $table->rememberToken();
+             $table->timestamps();
+         });
+     }
 
     /**
      * Reverse the migrations.
