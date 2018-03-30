b25c75c41adb10d8b43add715d8c85c176388df4
zsleader <522376982@qq.com>
2018-03-30 07:01:19 +0000

    userAuthorization

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+
+Route::get('example', function () {
+    return view('example');
+});
