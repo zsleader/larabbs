f85dbe272c90728ce5b7633a6d38da440f01520a
zsleader <522376982@qq.com>
2018-04-17 03:13:00 +0000

    topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'Laravel') - Laravel</title>
-
+    <meta name="description" content="@yield('description', 'laravel')" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
