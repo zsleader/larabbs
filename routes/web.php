551cfd93b0de06a7ee8a2e29331d992640c52c73
zsleader <522376982@qq.com>
2018-04-17 02:50:33 +0000

    use simditor and can upload image

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::get('example', function () {
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
